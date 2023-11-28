7db2fadb8c398e2590c37fa8732d46ab4b94267e
amiaxys <amyuqiliang@gmail.com>
2023-11-28 14:13:37 -0500

    fix: Mongo database tests

--- libs/langchain/tests/unit_tests/test_mongo_database.py
+++ libs/langchain/tests/unit_tests/test_mongo_database.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 
 from langchain.utilities.mongo_database import MongoDatabase
 
-uri = "mongodb://%2Ftmp%2Fmongodb-27017.sock/test_db?inMemory=true"
+uri = "mongodb://%2Ftmp%2Fmongodb-27017.sock/test_db"
 
 
 def test_collection_info() -> None:
